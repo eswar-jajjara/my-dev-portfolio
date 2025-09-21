8c97949e8de41780698766e752f719e10779c149
Eswar Vardan Jajjara <2300030262@kluniversity.in>
2025-09-21 12:14:51 +0530

    commit for output - @skks

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isProd ? "/my-dev-portfolio" : "",
+  assetPrefix: isProd ? "/my-dev-portfolio/" : "",
   images: {
     domains: ["cdn.sanity.io"],
-    unoptimized: true, // Required for static export with external images
+    unoptimized: true, 
   },
 };
 
